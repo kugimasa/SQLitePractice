b136e780f789ecfc051bdcffc5ede37df74c777b
kugi <kugimania@gmail.com>
2019-10-03 13:39:40 +0900

    create Player sql

---
+++ create-table-player.sql
@@ -0,0 +1,5 @@
+create table Player(
+pid char(3),
+pname char(8),
+score integer 
+);
---
+++ insert-player.sql
@@ -0,0 +1,6 @@
+-- insert 5 Player
+insert into Player values('000', 'Alex', 150);
+insert into Player values('001', 'Bob', 100);
+insert into Player values('002', 'Chad', 50);
+insert into Player values('003', 'Daivid', 80);
+insert into Player values('004', 'Edwin', 300);
